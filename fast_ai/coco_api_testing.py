5bde377abf268032b622a1fef36afb8c5808719d
Ed <red_parry@yahoo.co.uk>
2020-07-08 23:57:28 +0100

    copied the coco api example to experiment

---
+++ fast_ai/coco_api_testing.py
@@ -0,0 +1,48 @@
+from pycocotools.coco import COCO
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+
+#setup coco parameters
+
+dataType='val2017'
+annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
+
+# initialize COCO api for instance annotations
+coco=COCO(annFile)
+
+# display COCO categories and supercategories
+cats = coco.loadCats(coco.getCatIds())
+nms=[cat['name'] for cat in cats]
+print('COCO categories: \n{}\n'.format(' '.join(nms)))
+
+nms = set([cat['supercategory'] for cat in cats])
+print('COCO supercategories: \n{}'.format(' '.join(nms)))
+
+# get all images containing given categories, select one at random
+catIds = coco.getCatIds(catNms=['person','dog','skateboard']);
+imgIds = coco.getImgIds(catIds=catIds);
+imgIds = coco.getImgIds(imgIds = [324158])
+img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
+
+
+# load and display image
+# I = io.imread('%s/images/%s/%s'%(dataDir,dataType,img['file_name']))
+# use url to load image
+I = io.imread(img['coco_url'])
+plt.axis('off')
+plt.imshow(I)
+plt.show()
+
+
+# load and display instance annotations
+plt.imshow(I); plt.axis('off')
+annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
+anns = coco.loadAnns(annIds)
+coco.showAnns(anns)
+plt.show()
+
+
+
+
