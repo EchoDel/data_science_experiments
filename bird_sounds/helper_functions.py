a30132cdea077bd4f915dac0b94d413b33e35b77
redparry <red_parry@yahoo.co.uk>
2020-11-17 10:48:35 +0000

    added a basic import class for the bird metadatra

--- bird_sounds/helper_functions.py
+++ bird_sounds/helper_functions.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import math
+import torch
 
 from scipy.io import wavfile as wav
 from scipy.signal import spectrogram
@@ -26,6 +28,32 @@ def load_sound_file(path):
 
 def spectogram_creation(audio, sample_rate, samples):
     return spectrogram(audio, sample_rate, nperseg=64)
+
+
+class BirdCalls(torch.utils.data.IterableDataset):
+    def __init__(self, metadata_path, test, split_percentage=0.8, seed = 1994):
+        super(BirdCalls).__init__()
+        metadata = load_metadata(metadata_path)
+        metadata = metadata.sample(100).reset_index(drop=True)
+        np.random.seed(seed)
+        msk = np.random.rand(len(metadata)) < split_percentage
+        if test:
+            self.metadata = metadata[~msk].reset_index(drop=True)
+        else:
+            self.metadata = metadata[msk].reset_index(drop=True)
+        self.iteration_number = 0
+
+    def load_spectrogram(self, index):
+        data, rate = load_sound_file(self.metadata.iloc[index, 2])
+        frequency_graph = spectogram_creation(data, rate, None)
+        return frequency_graph[1]
+
+    def __iter__(self):
+        iteration = self.iteration_number
+        sample = torch.utils.data.DataLoader(self.load_spectrogram(iteration))
+        label = self.metadata.iloc[iteration, 1]
+        self.iteration_number += 1
+        yield (sample, label)
 
 
 
