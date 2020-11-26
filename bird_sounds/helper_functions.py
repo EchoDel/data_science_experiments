a5d391c95f8db27339df573fc0e2e368c3064a9f
redparry <red_parry@yahoo.co.uk>
2020-11-26 16:34:06 +0000

    changed the final layer to a tanh function to remove the nan errors occuring in later epochs

--- bird_sounds/helper_functions.py
+++ bird_sounds/helper_functions.py
@@ -160,7 +160,7 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
+            nn.Tanh(),
             nn.Linear(4096, num_classes),
         )
 
