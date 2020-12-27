825940d78aa55f1c8d675ea2794a05b4677e9cc6
Edward Parry <red_parry@yahoo.co.uk>
2020-12-27 11:34:56 +0000

    added the code needed to produce the outputs of the r value experiment

--- create_music/spectrogram/optimal_r_value.py
+++ create_music/spectrogram/optimal_r_value.py
@@ -6,13 +6,17 @@ import librosa
 import librosa.display
 from pathlib import Path
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 window_length = 2048
 window = windows.hamming(window_length, sym=False)
+csv_output = 'create_music/spectrogram/tracks_r_value.csv'
+AUDIO_DIR = Path('../data/fma_medium')
+fma_base = Path('fma/data/fma_metadata')
 
 
-def calculate_spacing(index, n_mels):
+def get_spectrogram(index, n_mels):
     filename = fmautils.get_audio_path(AUDIO_DIR, index)
 
     try:
@@ -29,6 +33,11 @@ def calculate_spacing(index, n_mels):
                                                  window=window,
                                                  power=1.0,
                                                  n_mels=n_mels)
+
+    return spectrogram
+
+def calculate_spacing(index, n_mels):
+    spectrogram = get_spectrogram(index, n_mels)
 
     optimal_r = []
 
@@ -51,11 +60,9 @@ def calculate_spacing(index, n_mels):
     return np.mean(optimal_r)
 
 
-AUDIO_DIR = Path('../data/fma_medium')
-
-tracks = fmautils.load('fma/data/fma_metadata/tracks.csv')
-genres = fmautils.load('fma/data/fma_metadata/genres.csv')
-features = fmautils.load('fma/data/fma_metadata/features.csv')
+tracks = fmautils.load(fma_base / 'tracks.csv')
+genres = fmautils.load(fma_base / 'genres.csv')
+features = fmautils.load(fma_base / 'features.csv')
 
 medium = tracks[tracks['set', 'subset'] <= 'medium']
 medium = medium.copy()
@@ -64,9 +71,27 @@ medium[('r_value', '128')] = medium.index.map(lambda x: calculate_spacing(x, n_m
 medium[('r_value', '256')] = medium.index.map(lambda x: calculate_spacing(x, n_mels=256))
 medium[('r_value', '376')] = medium.index.map(lambda x: calculate_spacing(x, n_mels=376))
 
-medium.to_csv('create_music/spectrogram/tracks_r_value.csv', index=False)
+medium.to_csv(csv_output, index=False)
 
+
+
+# Create outputs for the markdown
+medium = pd.read_csv(csv_output, header=[0, 1])
 
 medium.groupby(('track', 'genre_top')).agg({('r_value', '128'): 'mean',
                                             ('r_value', '256'): 'mean',
                                             ('r_value', '376'): 'mean'})
+
+spectrogram = get_spectrogram(2, 256)
+
+plt_index = 100
+plotting_data = spectrogram[:, plt_index]
+x_values = list(range(len(plotting_data)))
+smoothed_data = helper_functions.smooth(plotting_data, window_len=9)
+maximums = argrelextrema(smoothed_data, np.greater)[0]
+
+plt.plot(x_values, plotting_data)
+plt.plot(x_values, smoothed_data)
+plt.scatter(maximums, [plotting_data[i] for i in maximums], marker='o')
+plt.legend(['Spectrogram Slice', 'Smoothed Slice', 'Maximums'])
+plt.savefig(fname='create_music/spectrogram/contents/maximum_method.png')
