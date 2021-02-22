7dae0b9cd75483420dfa0d465f64f47ffdfe1978
redparry <red_parry@yahoo.co.uk>
2021-02-22 17:12:46 +0000

    added the basic functions to enable the autoencoder

---
+++ song_clustering/build_network.py
@@ -0,0 +1,113 @@
+import json
+from pathlib import Path
+import pandas as pd
+
+from song_clustering import helper_functions
+import torch
+from torch import nn
+from torch import optim
+from torchvision import transforms
+
+from matplotlib import pyplot as plt
+
+# load the metadata for the fma dataset
+sound_file_base = Path('E:/music')
+
+files = [x for x in sound_file_base.glob('**/*.*') if x.suffix in ['.mp3', '.m4a']]
+
+sound_files = pd.DataFrame({'names': [x.name for x in files],
+                            'path': files})
+sound_files = sound_files.sample(100)
+
+device = 'cuda'
+sample_length = 32768
+model_name = 'sound_file_clustering'
+metadata_file = 'spectrogram'
+epochs_to_run = 16000
+save_every = 100
+sample_rate = 22050
+window_length = 2048
+maximum_sample_location = 4096
+y_size = 500
+batch_size = 32
+
+
+config_file = Path(f'models/{metadata_file}/metadata_{model_name}.json')
+loader_path = Path(f'models/{metadata_file}/loader_{model_name}.pth')
+
+transformations = transforms.transforms.Compose([
+    # transforms.transforms.Normalize(mean=[0.485, 0.456, 0.406],
+    #                                 std=[0.229, 0.224, 0.225])
+])
+
+train_loader = torch.utils.data.DataLoader(
+    helper_functions.SongIngestion(sound_files,
+                                   sample_length=sample_length,
+                                   transformations=transformations,
+                                   sr=sample_rate,
+                                   window_length=window_length,
+                                   y_size=y_size,
+                                   n_mels=256,
+                                   maximum_sample_location=maximum_sample_location),
+    batch_size=batch_size)
+
+if config_file.exists():
+    with open(config_file, 'r') as outfile:
+        metadata = json.load(outfile)
+
+    for key, value in metadata.items():
+        if 'path' in value:
+            model_path = value['path']
+            starting_iteration = int(key)
+
+    model = torch.load(model_path)
+
+else:
+    model = helper_functions.AutoEncoder(song_identifier_inputs=len(train_loader.dataset),
+                                         sample_location_inputs=maximum_sample_location)
+    metadata = {}
+    starting_iteration = 0
+
+
+
+
+optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+criterion = nn.L1Loss()
+model.to(device)
+
+steps = 0
+running_loss = 0
+
+for epoch in range(epochs_to_run):
+    running_loss = 0
+    epoch = starting_iteration + epoch
+    model.train()
+    for results, song_identifier, sample_location in train_loader:
+        steps += 1
+        song_identifier, sample_location, results = \
+            song_identifier.to(device).float(), sample_location.to(device).float(), results.to(device)
+        optimizer.zero_grad()
+        logps = model(results)
+        loss = criterion(logps, results.type_as(logps))
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+    print(f"Epoch {epoch + 1}/{epochs_to_run + starting_iteration}.. "
+          f"Train loss: {running_loss / len(train_loader.dataset):.3f}.. ")
+
+    save_path = f'models/{metadata_file}/music_creation_{model_name}_{epoch + 1}.pth'
+    metadata[epoch + 1] = {
+        'running_loss': running_loss / len(train_loader.dataset),
+    }
+
+    if epoch % save_every == save_every - 1:
+        Path(save_path).parent.mkdir(exist_ok=True, parents=True)
+        metadata[epoch + 1]['path'] = save_path
+        torch.save(model, save_path)
+
+        # if loader_path.exists():
+        #     torch.save(train_loader, loader_path)
+
+        with open(config_file, 'w') as outfile:
+            json.dump(metadata, outfile)
---
+++ song_clustering/helper_functions.py
@@ -0,0 +1,208 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import random as rand
+import librosa
+import torch
+import torch.nn as nn
+from scipy.signal.windows import hamming
+from torch import tensor
+
+
+def smooth(x, window_len=11, window='hanning'):
+    """smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+
+    input:
+        x: the input signal
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+
+    see also:
+
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError("smooth only accepts 1 dimension arrays.")
+
+    if x.size < window_len:
+        raise ValueError("Input vector needs to be bigger than window size.")
+
+    if window_len < 3:
+        return x
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError("Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+
+    s = np.r_[x[window_len - 1:0:-1], x, x[-2:-window_len - 1:-1]]
+    # print(len(s))
+    if window == 'flat':  # moving average
+        w = np.ones(window_len, 'd')
+    else:
+        w = eval('np.' + window + '(window_len)')
+
+    y = np.convolve(w / w.sum(), s, mode='valid')
+    final_output = int(round(window_len/2))
+    return y[final_output:(final_output + len(x))]
+
+
+def load_sound_file(path, sr):
+    try:
+        data, rate = librosa.load(path,
+                                  sr=sr)
+
+    except Exception as e:
+        print(f"Reading of sample {path.name} failed")
+        print(e)
+
+    return data, rate
+
+
+def create_output(model: nn.Module, switches: list):
+    n_inputs = list(model.parameters())[0].shape[1]
+    inputs = np.zeros(n_inputs)
+
+    for key, value in switches:
+        inputs[key] = value
+
+    inputs = tensor(inputs).float()
+
+    output = model.features(inputs)
+    output = model.classifier(output)
+    return output.detach().numpy()
+
+
+class SongIngestion(torch.utils.data.Dataset):
+    def __init__(self, metadata, sample_length, transformations, sr, window_length,
+                 y_size, n_mels, maximum_sample_location, seed=1994):
+        super(SongIngestion).__init__()
+        self.metadata = metadata
+        self.n_mels = n_mels
+        self.maximum_sample_location = maximum_sample_location
+
+        rand.seed(seed)
+        np.random.seed(seed)
+
+        self.start = 0
+        self.end = self.metadata.shape[0]
+        self.y_size = y_size
+        self.sound_files = {}
+        self.n = 0
+        self.print_n = 0
+        self.length = sample_length
+        self.transformations = transformations
+        self.sr = sr
+        self.window_length = window_length
+        self.window = hamming(self.window_length, sym=False)
+
+    def onehot(self, n, maximum):
+        output = np.zeros(maximum)
+        output[n] = 1
+        return output
+
+    def load_sound_file(self, itemid):
+        if itemid not in self.sound_files:
+            if self.print_n % 100 == 0:
+                self.metadata.iloc[itemid, -1]
+            self.print_n += 1
+            self.sound_files[itemid] = load_sound_file(self.metadata.iloc[itemid, -1], self.sr)
+        return self.sound_files[itemid]
+
+    def load_spectrogram(self, data, rate):
+        frequency_graph = librosa.feature.melspectrogram(data,
+                                                         sr=rate,
+                                                         n_fft=self.window_length,
+                                                         hop_length=round(0.25 * self.window_length),
+                                                         window=self.window,
+                                                         n_mels=self.n_mels)
+        return frequency_graph
+
+    def pad_spectrogram(self, sample):
+        x, y = sample.shape
+        return np.pad(sample, ((0, self.n_mels - x), (0, self.y_size - y)))
+
+    def subsample(self, sample):
+        # Added a statement to correctly return samples under y_size long
+        if sample.shape[1] < self.y_size:
+            return self.pad_spectrogram(sample), 0
+        sample_length = sample.shape[1]
+        # start = rand.randint(0, sample_length - self.y_size)
+        # start = min(self.maximum_sample_location - 1, start)
+        start = 0
+        sample = sample[:, start:(start + self.y_size)]
+        return sample, start
+
+    def load_sample(self, index):
+        sample, rate = self.load_sound_file(index)
+        sample = self.load_spectrogram(sample, rate)
+        sample, start_index = self.subsample(sample)
+        # added a transpose to match the output of the neural network
+        sample = np.transpose(sample)
+        sample = tensor(sample).float()
+        sample = sample.view(1, self.y_size, self.n_mels)
+        sample = self.transformations(sample)
+        return sample, start_index
+
+    def __next__(self):
+        if self.n < self.end:
+            n = self.n
+            sample, one_hot, sample_location = self.__getitem__(n)
+            self.n += 1
+            return sample, one_hot, sample_location
+        else:
+            self.n = 0
+            raise StopIteration
+
+    def __getitem__(self, index):
+        sample, start_index = self.load_sample(index)
+        return sample, self.onehot(index, self.end), self.onehot(start_index, self.maximum_sample_location)
+
+    def __len__(self):
+        return self.end
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self, song_identifier_inputs, sample_location_inputs) -> None:
+        super(AutoEncoder, self).__init__()
+
+        self.encode = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
+            nn.Conv2d(16, 4, 3, stride=2, padding=1),  # b, 8, 3, 3
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
+        )
+
+        self.decode = nn.Sequential(
+            nn.ConvTranspose2d(4, 16, 3, stride=2),  # b, 16, 5, 5
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+            nn.Tanh()
+        )
+
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        x = self.encode(input_tensor)
+        x = self.decode(x)
+        return x
